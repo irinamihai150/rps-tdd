270f3a857ce70392eb87e80cacfd9bb915bc6302
irinamihai150 <irinamihai50@gmail.com>
2023-05-10 16:11:01 +0100

    Second test - scissors vs. rock

--- index.test.js
+++ index.test.js
@@ -6,17 +6,30 @@
 
 // 	expect(result).toBe(3);
 // });
-function rps() {
-  return "left";
+// function rps() {
+//   return "left";
+// }
+
+
+function rps(left) {
+	return left === "rock" ? "left" : "right";
 }
-
 describe("rock, paper, scissors", () => {
-	it("should say left wins for rock vs. scissors", () => {
-		const left = "rock";
-		const right = "scissors";
+	// it("should say left wins for rock vs. scissors", () => {
+	// 	const left = "rock";
+	// 	const right = "scissors";
 
-		const outcome = rps(left, right);
+	// 	const outcome = rps(left, right);
 
-		expect(outcome).toBe("left");
+	// 	expect(outcome).toBe("left");
+	// });
+
+  it("should say right wins for scissors vs. rock", () => {
+		const left = "scissors";
+		const right = "rock";
+
+		const result = rps(left, right);
+
+		expect(result).toBe("right");
 	});
 });
