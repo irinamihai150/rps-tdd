df375740cb15ee0fe7a93b6824fc91904a593685
irinamihai150 <irinamihai50@gmail.com>
2023-05-10 16:03:12 +0100

    First test - rock vs. scissors

---
+++ index.test.js
@@ -0,0 +1,22 @@
+// it("should work", () => {
+// 	const left = 1;
+// 	const right = 2;
+
+// 	const result = left + right;
+
+// 	expect(result).toBe(3);
+// });
+function rps() {
+  return "left";
+}
+
+describe("rock, paper, scissors", () => {
+	it("should say left wins for rock vs. scissors", () => {
+		const left = "rock";
+		const right = "scissors";
+
+		const outcome = rps(left, right);
+
+		expect(outcome).toBe("left");
+	});
+});
